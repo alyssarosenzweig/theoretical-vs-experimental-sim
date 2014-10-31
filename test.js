ac4d304347b5dd088aebb635726d5cb589844404
bobbybee <j104@rocketmail.com>
2014-10-31 15:53:40 -0400

    Awesome test

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 var e = new (require('./Experiment'))
 e.run = function() { return (Math.random() > 0.5) ? 1 : 0 }
 e.expected = function() { return 0.5; }
-e.iterateRun(100000)
-console.log(e.deviance());
+
+for(;;) {
+	e.iterate();
+	console.log(e.deviance());
+}
